93941056d7d6cb94e03b763fed34846a6e37d921
Nick Kezhaya <nick@whitepaperclip.com>
2021-05-02 10:17:41 -0500

    New expected emulator json

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -14,9 +14,9 @@ defmodule OnFlow.DataCase do
         service_credentials =
           OnFlow.Credentials.new!(%{
             address: "f8d6e0586b0a20c7",
-            private_key: "24523ed355d87bbf98e53cd1761892b09f06669f5751a23dfa9d6318650f08d5",
+            private_key: "d436bf5de20f752f1dfc709430c1164b8bf6fdb0f9564c1d6488885dd3513706",
             public_key:
-              "573d4d760d17c10c9f84879a4ebbcdd36a457d84764da2b1cfa7b1db1c59e7645d6ad97f92ab67fd2802fc5a3d7e5e1fee30f73916f0aa873ab7fd697f39e227"
+              "29d522b60bfb1ed1ccd9fb846295c4e25a8796a6e3de9da2100a6fceb353d86ae88688086c1817659c230fd589608da610eab24d3cb037b62cedb317b723c6f1"
           })
 
         [credentials: service_credentials]
