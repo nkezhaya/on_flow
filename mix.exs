f4fa99851bfe2cd658b3e11cf13ebc81b7fcf64d
Nick Kezhaya <nick@whitepaperclip.com>
2022-03-13 12:02:54 -0500

    Disable dialyzer runtime

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule OnFlow.MixProject do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
-      {:dialyxir, "~> 1.1", only: :dev},
+      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:grpc, ">= 0.0.0"},
       {:cowlib, ">= 0.0.0"},
       {:jason, ">= 0.0.0"},
